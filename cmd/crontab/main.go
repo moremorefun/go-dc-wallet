0ba889f7e191e38216cf2046b890b10843fece09
hao <unnamed.hao@gmail.com>
2020-06-29 15:08:35 +0800

    add eos

--- cmd/crontab/main.go
+++ cmd/crontab/main.go
@@ -5,6 +5,7 @@ import (
 	"go-dc-wallet/app"
 	"go-dc-wallet/hbtc"
 	"go-dc-wallet/hcommon"
+	"go-dc-wallet/heos"
 	"go-dc-wallet/heth"
 
 	"github.com/robfig/cron/v3"
@@ -151,6 +152,38 @@ func main() {
 	}
 	// 检测 omni 通知到账
 	_, err = c.AddFunc("@every 5s", hbtc.OmniCheckTxNotify)
+	if err != nil {
+		hcommon.Log.Errorf("cron add func error: %#v", err)
+	}
+
+	// --- eos ---
+	// 检测 eos 生成地址
+	_, err = c.AddFunc("@every 1m", heos.CheckAddressFree)
+	if err != nil {
+		hcommon.Log.Errorf("cron add func error: %#v", err)
+	}
+	// 检测 eos 冲币
+	_, err = c.AddFunc("@every 3s", heos.CheckBlockSeek)
+	if err != nil {
+		hcommon.Log.Errorf("cron add func error: %#v", err)
+	}
+	// 检测 eos 提币
+	_, err = c.AddFunc("@every 3m", heos.CheckWithdraw)
+	if err != nil {
+		hcommon.Log.Errorf("cron add func error: %#v", err)
+	}
+	// 检测 eos 发送交易
+	_, err = c.AddFunc("@every 1s", heos.CheckRawTxSend)
+	if err != nil {
+		hcommon.Log.Errorf("cron add func error: %#v", err)
+	}
+	// 检测 eos 交易上链
+	_, err = c.AddFunc("@every 3s", heos.CheckRawTxConfirm)
+	if err != nil {
+		hcommon.Log.Errorf("cron add func error: %#v", err)
+	}
+	// 检测 eos 通知到账
+	_, err = c.AddFunc("@every 3s", heos.CheckTxNotify)
 	if err != nil {
 		hcommon.Log.Errorf("cron add func error: %#v", err)
 	}
